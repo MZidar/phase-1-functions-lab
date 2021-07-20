03eca4b8b349ac6a2feaed04bae8936d27901caf
Michael Zidar <mzidar@gmail.com>
2021-07-19 23:03:47 -0400

    commit tree f719ad4f392e59ed331373a9bf9012a081b18043

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block){
+    if(block > 42){
+        return (block-42);
+    }
+    else{
+        return (42-block);
+    }
+}
+
+function distanceFromHqInFeet(block){
+    if(block > 42){
+        return (block-42)*264;
+    }
+    else{
+        return (42-block)*264;
+    }
+}
+
+function distanceTravelledInFeet(block, block2){
+    if(block < block2){
+        return (block2-block)*264;
+    }
+    else{
+        return (block-block2)*264;
+    }
+}
+
+function calculatesFarePrice(block, block2){
+    if(distanceTravelledInFeet(block, block2) < 400){
+        return 0;
+    }
+    else if(distanceTravelledInFeet(block,block2) <= 2000){
+        return (distanceTravelledInFeet(block, block2)-400)*.02;
+    }
+    else if(distanceTravelledInFeet(block,block2) <= 2500){
+    return 25;
+    }
+    else if(distanceTravelledInFeet(block,block2) > 2500){
+        return 'cannot travel that far';
+    }
+}
